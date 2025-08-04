9a8cbb1620d3f01e6afbabfd1939ac89aa7500c7
tuyisenge250 <benjaminwell250@gmail.com>
2025-08-04 12:33:10 +0200

    implement function to convert text

--- 6kyu/alphabetPosition.js
+++ 6kyu/alphabetPosition.js
@@ -1,3 +1,3 @@
-function duplicateEncode(word){
-    return word.toLowerCase().split("").map((x,i, arr) => arr.lastIndexOf(x) === i && arr.indexOf(x) === i ? "(" : ")").join("");
+function alphabetPosition(text) {
+  return text.toLowerCase().split("").map(x => "abcdefghijklmnopqrstuvwxyz".indexOf(x) + 1).filter(x => x != 0).join(" ");
 }
